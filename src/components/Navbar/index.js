06acf1d6fe0c4a6f42e579305d54cbed80782029
joycethha <joycethh@outlook.com>
2022-07-01 16:20:47 -0500

    complete Navbar styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
 
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElement";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElement";
 
 function Navbar() {
   return (
@@ -8,7 +19,26 @@ function Navbar() {
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">Tang</NavLogo>
-          <h1>Hey</h1>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="Service">Service</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
