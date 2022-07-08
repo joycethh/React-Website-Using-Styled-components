d22ab7b7a5b7cf1b38a62ecd629b4b3967a4f92d
joycethha <joycethh@outlook.com>
2022-07-08 11:27:59 -0500

    create ServiceSection

---
+++ src/components/Services/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  ServicesContainer,
+  ServiceH1,
+  ServiceWarp,
+  ServiceCard,
+  ServiceIcon,
+  ServiceH2,
+  ServiceP,
+} from "./ServiceElement";
+
+import Icon1 from "../../images/svg-saving.svg";
+import Icon2 from "../../images/svg-sucess.svg";
+import Icon3 from "../../images/svg-file.svg";
+const Service = () => {
+  return (
+    <>
+      <ServicesContainer>
+        <ServiceH1>Our Services</ServiceH1>
+        <ServiceWarp>
+          <ServiceCard>
+            <ServiceIcon src={Icon1} />
+            <ServiceH2>Reduce Expense</ServiceH2>
+            <ServiceP>
+              We help reduce your fees and increase your revenues
+            </ServiceP>
+          </ServiceCard>
+
+          <ServiceCard>
+            <ServiceIcon src={Icon2} />
+            <ServiceH2>Virtual Office</ServiceH2>
+            <ServiceP>
+              You can access our platform anywhere in the world.
+            </ServiceP>
+          </ServiceCard>
+
+          <ServiceCard>
+            <ServiceIcon src={Icon3} />
+            <ServiceH2>Premium Benfits</ServiceH2>
+            <ServiceP>
+              Unlock our special membership card that offers 5% cash back.
+            </ServiceP>
+          </ServiceCard>
+        </ServiceWarp>
+      </ServicesContainer>
+    </>
+  );
+};
+
+export default Service;
